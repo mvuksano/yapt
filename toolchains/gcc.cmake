1d0989d6afaf335beacde92d53d459dcd183e270
Marko Vuksanovic <markovuksanovic@gmail.com>
2023-03-16 00:05:41 -0700

    Conditionally find gcc or gcc-12 and g++ or g++-12 binaries.

--- toolchains/gcc.cmake
+++ toolchains/gcc.cmake
@@ -1,5 +1,17 @@
-set(CMAKE_C_COMPILER "/usr/bin/gcc-12")
-set(CMAKE_CXX_COMPILER "/usr/bin/g++12")
+find_program(GCC_COMPILER gcc gcc-12)
+
+if(GCC_COMPILER)
+    set(CMAKE_C_COMPILER ${GCC_COMPILER})
+else()
+    message(FATAL_ERROR "gcc compiler not found")
+endif()
+
+find_program(GPP_COMPILER g++ g++-12)
+if(GPP_COMPILER)
+    set(CMAKE_CXX_COMPILER ${GPP_COMPILER})
+else()
+    message(FATAL_ERROR "g++ compiler not found")
+endif()
 
 set(CMAKE_CXX_FLAGS "-O2 -g3")
 set(CMAKE_CXX_STANDARD 20)
@@ -8,4 +20,3 @@ set(CMAKE_BUILD_TYPE RelWithDebInfo)
 if(${PORT} MATCHES "sqlite3")
     set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 endif()
-
