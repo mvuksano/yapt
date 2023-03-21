dd32967d15064f50fc6ca5abfe604d63df6e9b22
Marko Vuksanovic <markovuksanovic@gmail.com>
2023-03-21 09:55:18 -0700

    Add comment about how to save templates generated by C++.

--- toolchains/gcc.cmake
+++ toolchains/gcc.cmake
@@ -13,8 +13,14 @@ else()
     message(FATAL_ERROR "g++ compiler not found")
 endif()
 
+# To see generated templates add -save-temps=obj flag.
 set(CMAKE_CXX_FLAGS "-O2 -g3")
 set(CMAKE_CXX_STANDARD 20)
+
+find_program(MOLD_LINKER mold)
+if(MOLD_LINKER)
+    set(CMAKE_CXX_FLAGS "-fuse-ld=mold")
+endif()
 set(CMAKE_BUILD_TYPE RelWithDebInfo)
 
 if(${PORT} MATCHES "sqlite3")
